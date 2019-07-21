d7b68daab7d5e4d09a07285e44dc61e902d9a432
shaoxuehai <shaoxuehai@gridsum.com>
2019-07-21 13:54:47 +0800

    init SmartTaskbar

---
+++ SmartTask/Main.cs
@@ -0,0 +1,92 @@
+﻿using SmartTask.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Win32ApI;
+
+namespace SmartTask
+{
+    public partial class MainForm : Form
+    {
+        private bool currentWindowMax = false;
+
+        [DllImport("user32.dll")]
+        public static extern bool IsZoomed(IntPtr hWnd);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            Hide();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            Task.Factory.StartNew(() =>
+            {
+                for (; ; )
+                {
+                    IntPtr hWnd = User32.GetForegroundWindowProcessId(out int calcID);
+                    if (hWnd != IntPtr.Zero)
+                    {
+                        bool isZoomed = IsZoomed(hWnd);
+                        if (currentWindowMax != isZoomed)
+                        {
+                            SwichTaskBar(IsZoomed(hWnd));
+                            currentWindowMax = isZoomed;
+                        }
+                    }
+                    Thread.Sleep(500);
+                }
+            });
+
+        }
+
+        private void SwichTaskBar(bool isHide)
+        {
+            TaskbarController.SetTaskbarState(isHide ? 
+                TaskbarController.AppBarStates.AutoHide : 
+                TaskbarController.AppBarStates.AlwaysOnTop);
+        }
+
+        private void RunStart_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!(RunStart.Checked ^ Program.IsRunStart())) return;
+            if (Program.RunWhenStart(RunStart.Checked))
+            {
+                Settings.Default.RunStart = RunStart.Checked;
+                Settings.Default.Save();
+            }
+            else
+            {
+                RunStart.Checked = !RunStart.Checked;
+            }
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Exit();
+        }
+    }
+}
---
+++ SmartTask/Program.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Win32ApI;
+
+namespace SmartTask
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            /*
+             * 设置管理员启动 
+             */
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+#if DEBUG
+            Application.Run(new MainForm());
+#else
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    WorkingDirectory = Environment.CurrentDirectory,
+                    FileName = Application.ExecutablePath,
+                    Verb = "runas" 
+                };
+                try
+                {
+                    Process.Start(startInfo);
+                }
+                catch 
+                {
+                    return;
+                }
+            }
+#endif
+        }
+
+        const string REGISTRY_KEY_RUN_PATH = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+        public static bool RunWhenStart(bool Started)
+        {
+            bool result = true;
+            string productName = Application.ProductName;
+            RegistryKey HKLM = Registry.LocalMachine;
+            RegistryKey Run = HKLM.CreateSubKey(REGISTRY_KEY_RUN_PATH);
+            if (Started)
+            {
+                try
+                {
+                    Run.SetValue(productName, Path.GetFullPath(Application.ExecutablePath));
+                }
+                catch (Exception err)
+                {
+                    result = false;
+                    MessageBox.Show(err.Message.ToString(), productName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    HKLM.Close();
+                    Run.Close();
+                }
+            }
+            else
+            {
+                try
+                {
+                    Run.DeleteValue(productName, false);
+                }
+                catch (Exception err)
+                {
+                    result = false;
+                    MessageBox.Show(err.Message.ToString(), productName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } 
+                finally
+                {
+                    HKLM.Close();
+                    Run.Close();
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 是否为开机启动
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsRunStart()
+        {
+            string productName = Application.ProductName;
+            RegistryKey HKLM = Registry.LocalMachine;
+            RegistryKey Run = HKLM.CreateSubKey(REGISTRY_KEY_RUN_PATH);
+            try
+            {
+                return Run.GetValueNames().Any(k => k == productName);
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+    }
+}
